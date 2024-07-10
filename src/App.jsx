46d68fee6ec897d2a9d483d551e6f7379c80fc1d
zaidrauf7 <iamzaidrauf@gmail.com>
2024-07-10 12:50:39 +0500

    signup authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './globals.css'
 import { Route, Routes } from 'react-router-dom'
+import SignUp from './pages/SignUp'
 
 function App() {
 
@@ -8,8 +9,8 @@ function App() {
     <>
 <Routes>
 {/* <Route exact path='/' element={<Home/>} /> */}
-{/* <Route exact path='/login' element={<Login/>} />
-<Route exact path='/signup' element={<Signup/>} /> */}
+{/* <Route exact path='/login' element={<Login/>} /> */}
+<Route exact path='/signup' element={<SignUp/>} />
 
 </Routes>
 
