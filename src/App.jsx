c489d18632e92f7982d1634ced3a00e152446f2a
AhmadMahmood-123 <techexpert10885@gmail.com>
2024-07-10 20:22:50 +0500

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './globals.css'
 import { Route, Routes } from 'react-router-dom'
 import SignUp from './pages/SignUp'
+import { Layout } from './pages/Layout'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
 {/* <Route exact path='/' element={<Home/>} /> */}
 {/* <Route exact path='/login' element={<Login/>} /> */}
 <Route exact path='/signup' element={<SignUp/>} />
+<Route exact path='/' element={<Layout/>} />
 
 </Routes>
 
