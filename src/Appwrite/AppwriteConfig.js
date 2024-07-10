a4855894f6e5a84057955bf11c4cbc2c94c5eb0c
AhmadMahmood-123 <techexpert10885@gmail.com>
2024-07-10 12:27:37 +0500

    Appwrite_Configuration

---
+++ src/Appwrite/AppwriteConfig.js
@@ -0,0 +1,21 @@
+import { Account, Client ,Databases, ID,Storage } from 'appwrite';
+
+const client = new Client();
+export const PROJECT_ID = "668e310c002b228c8df2"
+export const DATABASE_ID = "668e3750000454fe192e"
+export const COLLECTION_ID_DATA = "668e37900010470157b2"
+
+client
+
+.setEndpoint('https://cloud.appwrite.io/v1')
+.setProject('668e310c002b228c8df2');
+
+export const storage = new Storage(client);
+export const databases = new Databases(client);
+export const account = new Account(client);
+export { ID } from "appwrite";
+
+
+
+
+export default client;
