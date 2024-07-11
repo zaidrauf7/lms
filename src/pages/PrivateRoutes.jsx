ccb8d5f3f449ff3101e76fc85c2513fea8d45985
zaidrauf7 <iamzaidrauf@gmail.com>
2024-07-11 15:35:05 +0500

    private routes

--- src/pages/PrivateRoutes.jsx
+++ src/pages/PrivateRoutes.jsx
@@ -6,9 +6,9 @@ import useLocalStorage from "@/hooks/useLocalStorage";
 const PrivateRoutes = () => {
   const { getItem } = useLocalStorage();
 
-  // const user = getItem("cookieFallback");
-  const user = 44
-  return user ? (
+  const user = getItem("cookieFallback");
+  // const user = 44
+  return user.a_session_666191a00026fbe3a2bc ? (
     <Layout>
       <Outlet />
     </Layout>
