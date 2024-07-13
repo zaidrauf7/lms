7b7a0a145aebfd08ce4bdb44142312f4b0803c69
AhmadMahmood-123 <techexpert10885@gmail.com>
2024-07-13 13:03:35 +0500

    update public route

--- src/pages/PublicRoutes.jsx
+++ src/pages/PublicRoutes.jsx
@@ -5,7 +5,7 @@ import { Navigate, Outlet } from "react-router-dom";
 const PublicRoutes = () => {
   const { getItem } = useLocalStorage();
   const user = getItem("cookieFallback");
-  return user.a_session_668e310c002b228c8df2 ? (
+  return user ? (
     <Navigate to={"/"} />
   ) : (
     <Outlet />
