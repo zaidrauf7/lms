a746a99df843c467fa92d3b9b778c3df5dffca29
AhmadMahmood-123 <techexpert10885@gmail.com>
2024-07-15 19:26:05 +0500

    set data

--- src/Appwrite/AppwriteConfig.js
+++ src/Appwrite/AppwriteConfig.js
@@ -3,7 +3,7 @@ import { Account, Client ,Databases, ID,Storage } from 'appwrite';
 const client = new Client();
 export const PROJECT_ID = "668e310c002b228c8df2"
 export const DATABASE_ID = "668e3750000454fe192e"
-export const COLLECTION_ID_DATA = "668e37900010470157b2"
+export const COLLECTION_ID_STUDENTDATA = "668e37900010470157b2"
 export const COLLECTION_ID_USERDATA = "669010c80028e423703a"
 
 
