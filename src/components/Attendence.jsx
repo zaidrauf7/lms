47509fac20ab45d148c82ff963deb60d81a7c4eb
zaidrauf7 <iamzaidrauf@gmail.com>
2024-08-01 22:39:50 +0500

    attendence

--- src/components/Attendence.jsx
+++ src/components/Attendence.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import {
+  COLLECTION_ID_STUDENTATTENDENCE,
+  DATABASE_ID,
+  databases,
+} from "@/Appwrite/AppwriteConfig";
+import React, { useEffect, useState } from "react";
 
 const Attendence = () => {
-  return (
-    <div>Attendence</div>
-  )
-}
+  const [getData, setGetData] = useState([])
+  const attendence = async () => {
+    const result = await databases.listDocuments(
+      DATABASE_ID,
+      COLLECTION_ID_STUDENTATTENDENCE,
 
-export default Attendence
+    );
+    setGetData(result.documents)
+  };
+  useEffect(()=>{
+    attendence()
+  },[])
+  console.log(getData);
+  return <div>
+    {getData.map((val) =>{
+      return(
+       <div className="flex justify-between gap-10">
+         <h1>{val.name}</h1>
+         <h1>{val.date.slice(0 , 10)}</h1>
+         <h1>{val.intime}</h1>
+       </div>
+      )
+    })}
+  </div>;
+};
+
+export default Attendence;
